0019ab86f4621031af33990242aa25b0e096d1ee
Voamorim <135454159+Voamorim@users.noreply.github.com>
2023-12-12 18:52:31 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/**
+ * Bomberman Minecraft, 
+ * 
+ * por: Pedro Barreto e Vítor Amorim
+ * 
+ * Trabalho prático AEDS1 2023-2
+*/
+
 #include "raylib.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -30,11 +38,12 @@ int main(void)
     InitGame(&game);
     InitAudioDevice();
 
+    /* Inicializa as variáveis de som */
     Sound click = LoadSound("sprites/audio/click.mp3");
     Music initial_music = LoadMusicStream("sprites/audio/initial_music.mp3");
     Sound win_sound = LoadSound("sprites/audio/challenge_complete.mp3");
     Sound tnt_sound = LoadSound("sprites/audio/tnt-explosion.mp3");
-
+    /* Parte responsável pela tela inicial do jogo*/
     PlayMusicStream(initial_music);
     int aux = 0;
     while(!game.start){
@@ -43,7 +52,10 @@ int main(void)
     }
     PauseMusicStream(initial_music);
     UnloadMusicStream(initial_music);
+
     map0_setup(&game);
+
+    /* Parte responsável por criar as strings responsáveis pelos placares a serem exibidos */
     char vencedor2[40] = "VENCEU ";
     strcat(vencedor2, game.hero2.nick);
     strcat(vencedor2, "!");
@@ -59,19 +71,26 @@ int main(void)
     vitorias1[1] = '\0';
     vitorias2[0] = vitorias2c;
     vitorias2[1] = '\0';
+
+    /* Atualiza o jogo enquanto os jogadores não decretam gameover pela tela de revanche */
     while(!game.gameover){
         Music game_music = LoadMusicStream("sprites/audio/in_game_music1.ogg");
         srand(time(NULL));
+        /* Responsável por conseguir o tempo passado desde o início do programa, para depois possibilitar que sempre o jogo tenha um tempo limite de 120 segundos */
         float tempo_desde_inicio = GetTime();
         int aux_tempo = tempo_desde_inicio;
+
         while (game.start)
         {
             PlayMusicStream(game_music);
             UpdateMusicStream(game_music);
             DrawTexture(game.telaJogoMapa1, 0, 0, RAYWHITE);
+            /* Atualiza a contagem regressiva de acordo com o tempo que se passou desde a entrada no jogo */
             float current_time = GetTime();
             game.countdown_value = COUNTDOWN_DURATION - (int) current_time + aux_tempo;
+
             UpdateDrawFrame(&game, tnt_sound);
+            /* Termina o jogo caso peo menos um dos jogadores tenha morrido ou o tempo tenha se expirado */
             if(!game.hero.isAlive || !game.hero2.isAlive || (game.countdown_value <= -5)){
                 game.start = 0;
                 PlaySound(win_sound);
@@ -81,6 +100,7 @@ int main(void)
         PauseMusicStream(game_music);
         UnloadMusicStream(game_music);
         Music revenge_music = LoadMusicStream("sprites/audio/revenge_music.mp3");
+        /* Parte responsável por atualizar o placar do jogo */
         char vitorias1c = '0' + game.hero.vitorias;
         char vitorias2c =  '0' + game.hero2.vitorias;
         vitorias1[0] = vitorias1c;
@@ -93,16 +113,19 @@ int main(void)
         strcat(placar, vitorias1);
         strcat(placar, " x ");
         strcat(placar, vitorias2);
+        /* Faz a tela de revanche */
         PlayMusicStream(revenge_music);
         while(!IsKeyDown(KEY_ENTER)){
             BeginDrawing();
             UpdateMusicStream(revenge_music);
             ClearBackground(RAYWHITE);
             DrawTexture(game.backgroundRevanche, 0, 0, RAYWHITE);
+            /* Cria os botões de gameover e de revanche */
             Rectangle gameover_button = {32, 430, 218, 52};
             Rectangle revenge_button = {558, 430, 218, 52};
+            /* Escreve o placar do jogo */
             DrawText(placar, (GetScreenWidth()/2 - MeasureText("EMPATE", 80)/2 + 80), 230, 80, WHITE);
-
+            /* Escreve quem ganhou a partida */
             if((game.hero.isAlive == 0 && game.hero2.isAlive == 0) || (game.hero.isAlive == 1 && game.hero2.isAlive == 1)){
                 DrawText("EMPATE!", (GetScreenWidth()/2 - MeasureText("EMPATE", 80)/2 + 40), 80, 60, WHITE);
             } else if(game.hero.isAlive == 0){
@@ -110,13 +133,16 @@ int main(void)
             } else{
                 DrawText(vencedor1, (GetScreenWidth()/2 - MeasureText(vencedor1, 80)/2 + 80), 80, 60, WHITE);
             }
+            /* Detecta se um dos jogadores apertou a tecla ESC para fechar a tela do jogo */
             if(IsKeyPressed(KEY_ESCAPE)){
                 game.gameover = 1;
                 break;
             }
+            /* Detecta se o usuário apertou o botão esquerdo do mouse em cima de um dos botões da tela de revanche*/
             if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
                 if(CheckCollisionPointRec(GetMousePosition(), gameover_button)){
                     PlaySound(click);
+                    /* Atualiza o arquivo placar.txt com o placar do jogo */
                     FILE *file = fopen("placar.txt", "a");
                     fputs(placar, file);
                     fputc('\n', file);
